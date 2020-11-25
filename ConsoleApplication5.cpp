8bf117a61902f0a27b748058b6f2737d7e2fe724
gir-ctrl <61008882+gir-ctrl@users.noreply.github.com>
2020-11-25 18:35:28 +0300

    Update ConsoleApplication5.cpp

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlib.h>#include <stdlib.h>
 #include <math.h>
 #include "glut.h"
 #include <list>
@@ -21,14 +21,13 @@ public:
 		y += dot.y;
 		return *this;
 	}
-	void DrawPoint(Point& MyPoint);
+	void DrawPoint() { 
+		glBegin(GL_POINTS); 
+		glColor3d(0, 1, 0);
+		glVertex2d(x, y); 
+		glEnd();
+	};
 };
-//void Point::DrawPoint(Point& MyPoint) {
-//	glBegin(GL_POINTS);
-//	glColor3d(0, 1, 0);
-//	glVertex2d(MyPoint.x, MyPoint.y);
-//	glEnd();
-//}
 class Line {
 private:
 	Point a;
@@ -42,9 +41,32 @@ public:
 		b += dot.b;
 		return *this;
 	}
-	void DrawLine(Line& Myline);
-	void DrawTrace(Line& Myline, Line& MyTrace);
-	void doing(Line& Myline, Line& MyTrace);
+	void DrawLine(Line& Myline) {
+		glLineWidth(3);
+		glBegin(GL_LINES);
+		glColor3d(0, 1, 0);
+		a.DrawPoint();
+		b.DrawPoint();
+		glEnd();
+	};
+	void DrawTrace(Line& Myline, Line& MyTrace) {
+		glLineWidth(3);
+		glBegin(GL_POLYGON);
+		glColor3d(1, 0, 0);
+		Myline.a.DrawPoint();
+		Myline.b.DrawPoint();
+		MyTrace.a.DrawPoint();
+		MyTrace.b.DrawPoint();
+		glEnd();
+	};
+	void doing(Line& Myline, Line& MyTrace) {
+		if (Myline.a.Getx() <= -3 || Myline.a.Getx() >= 3 || Myline.b.Getx() <= -4 || Myline.b.Getx() >= 3) {
+			MyTrace.NegA();
+		}
+		else if (Myline.a.Gety() <= -3 || Myline.a.Gety() >= 3 || Myline.b.Gety() <= -4 || Myline.b.Gety() >= 3) {
+			MyTrace.NegB();
+		}
+	};
 };
 void changeSize(int w, int h) {
 	if (h == 0)
@@ -56,40 +78,15 @@ void changeSize(int w, int h) {
 	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
 	glMatrixMode(GL_MODELVIEW);
 }
-void Line::DrawLine(Line& Myline) {
-	glLineWidth(3);
-	glBegin(GL_LINES);
-	glColor3d(0, 1, 0);
-	glVertex2d(Myline.a.Getx(), Myline.a.GetY());
-	glVertex2d(Myline.b.GetX(), Myline.b.GetY());
-	glEnd();
-}
-void Line::DrawTrace(Line& Myline, Line& MyTrace) {
-	glLineWidth(3);
-	glBegin(GL_POLYGON);
-	glColor3d(1, 0, 0);
-	glVertex2d(Myline.a.Getx(), Myline.a.Gety());
-	glVertex2d(Myline.b.Getx(), Myline.b.Gety());
-	glVertex2d(Myline.a.Getx() - 5 * MyTrace.a.Getx(), Myline.a.Gety() - 5 * MyTrace.a.Gety());
-	glVertex2d(Myline.b.Getx() - 5 * MyTrace.b.Getx(), Myline.b.Gety() - 5 * MyTrace.b.Gety());
-	glEnd();
-}
-void Line::doing(Line& Myline, Line& MyTrace) {
-	if (Myline.a.Getx() <= -3 || Myline.a.Getx() >= 3 || Myline.b.Getx() <= -4 || Myline.b.Getx() >= 3) {
-		MyTrace.NegA();
-	}
-	else if (Myline.a.Gety() <= -3 || Myline.a.Gety() >= 3 || Myline.b.Gety() <= -4 || Myline.b.Gety() >= 3) {
-		MyTrace.NegB();
-	}
-}
 Line Myline = Line(Point(0, 0), Point(1, 1));
 Line MyTrace = Line(Point(0.01, 0.01), Point(0.01, 0.01));
+Line MyTrace2 = Line(Point(0.95, 0.95), Point(0.05, 0.05));
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 	gluLookAt(0.0f, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.1f, 0.0f);
 	Myline.DrawLine(Myline);
-	Myline.DrawTrace(Myline, MyTrace);
+	Myline.DrawTrace(Myline, MyTrace2);
 	Myline += MyTrace;
 	Myline.doing(Myline, MyTrace);
 	glutSwapBuffers();
