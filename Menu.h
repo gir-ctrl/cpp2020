7a8a59751e879f271fde1efef48c498d3c04eda9
gir-ctrl <61008882+gir-ctrl@users.noreply.github.com>
2020-12-17 14:21:27 +0300

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -11,17 +11,82 @@
 #include <vector>
 using namespace std;
 
-struct rectangle {
+class rectangle {
+private:
 	double x1, y1, x2, y2;
+public:
+	rectangle(){ x1 = 0; y1 = 0; x2 = 0; y2 = 0; }
+	rectangle(double a1, double b1, double a2, double b2){
+		x1 = a1; y1 = b1;
+		x2 = a2; y2 = b2;
+	}
+	void rectDraw(float r, float g, float b){
+		glBegin(GL_QUADS);
+		glColor3f(r, g, b);
+		glVertex2f(x1, y1);
+		glVertex2f(x2, y1);
+		glVertex2f(x2, y2);
+		glVertex2f(x1, y2);
+		glEnd();
+		glBegin(GL_LINE_STRIP);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		glVertex2f(x1, y1);
+		glVertex2f(x2, y1);
+		glVertex2f(x2, y2);
+		glVertex2f(x1, y2);
+		glVertex2f(x1, y1);
+		glEnd();
+	};
 };
-struct button {
+class button {
+private:
 	rectangle coords;
 	string text;
+public:
+	button(double a1, double b1, double a2, double b2, string text_){
+		coords = rectangle(a1, b1, a2, b2);
+		text = text_;
+	}
+	void butDraw(bool act){
+		if (act == true)
+			coords.rectDraw(0.4, 0.4, 0.4);
+		else
+			coords.rectDraw(0.66, 0.66, 0.66);
+	}
+	string get_text(){ return text; };
 };
-struct menu {
+typedef enum { up, down } direction;
+class menu {
+private:
 	rectangle coordinates;
 	vector<button> key;
 	int active;
+public:
+	menu(double a1, double b1, double a2, double b2){
+		coordinates = rectangle(a1, b1, a2, b2);
+		active = 0;
+	};
+	void addition(double a1, double b1, double a2, double b2, string text_){
+		key.push_back(button(a1, b1, a2, b2, text_));
+	}
+	void menuDraw(){
+		coordinates.rectDraw(0.9, 0.9, 0.9);
+		int i = 0;
+		for (auto& cur_b : key){
+			if (i == active)
+				cur_b.butDraw(true);
+			else
+				cur_b.butDraw(false);
+			i++;
+		}
+	};
+	string menuAction(){ return key[active].get_text(); };
+	void move(direction dir){
+		if ((dir == down) && (active < key.size() - 1))
+			active++;
+		if ((dir == up) && (active > 0))
+			active--;
+	}
 };
 vector<menu> MenuRead(string name_file);
 void InitMenu(string name_file);
@@ -30,4 +95,3 @@ menu* CurrentMenu();
 void OpenMenu(int a);
 void CloseMenu();
 void DrawMenu(void);
-vector<menu> MenuRead(string name_file);
